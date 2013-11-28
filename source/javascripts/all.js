52a568c1757a64b5240cbae89df444b4895752df
Steve Books <stevebooks@gmail.com>
2013-11-28 08:20:51 -0700

    Initial Commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require ionic
+//= require angular/angular
+//= require angular/angular-animate
+//= require angular/angular-route
+//= require angular/angular-touch
+//= require angular/angular-sanitize
+//= require ionic-angular
+//= require_tree .
